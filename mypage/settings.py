5d910d1e65455279b988dc096c8cef6b6821362b
wastijeet110@gmail.com <ClinkRatCheat>
2023-06-11 20:43:24 +0530

    Pio

--- mypage/settings.py
+++ mypage/settings.py
@@ -192,4 +192,4 @@ EMAIL_HOST_PASSWORD='xmgxmfkcnnsandtp'
 import os
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
